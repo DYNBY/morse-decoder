ae5a7402b712e71f382a8f8401e53bb0ee9c4239
dynby <dyominyuri@gmail.com>
2023-02-09 22:02:28 +0300

    morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const length = 10;
+    const pattern = new RegExp(".{1," + length + "}", "ig");
+    let res = expr.match(pattern).map(item => item.padEnd(length, "."));
+    for(let key in res){
+        res[key] = res[key].slice(res[key].indexOf('1'))
+        res[key] = res[key].replaceAll('10',".")
+        res[key] = res[key].replaceAll('11',"-")
+        res[key] = res[key].replaceAll('*'," ")
+        let k = 0;
+        for(let i of Object.keys(MORSE_TABLE)){
+            k = k + 1;
+            if(i === res[key]){
+                res[key] = res[key].replace(res[key], Object.values(MORSE_TABLE)[k-1]);
+            }}}
+    let result = res.join('');
+    return result;
 }
 
 module.exports = {
